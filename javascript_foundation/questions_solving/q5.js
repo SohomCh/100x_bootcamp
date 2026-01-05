7180bc29ac18f5be61f1282f62ba096af418bde9
Sohom Chatterjee <sohom12345678910@gmail.com>
2026-01-05 19:05:11 +0530

    js quessolving

---
+++ javascript_foundation/questions_solving/q5.js
@@ -0,0 +1,35 @@
+/**Q5. Promise resolve / reject
+
+Create a function checkAge(age) that:
+
+Resolves if age ≥ 18 → "Allowed"
+
+Rejects otherwise → "Not allowed"
+
+Test both cases. */
+
+
+
+
+function checkAge(age) {
+    return new Promise((resolve, reject) => {
+        if (age >= 18) {
+            resolve("Allowed");
+        } else {
+            reject("Not allowed");
+        }
+    });
+}
+
+checkAge(20)
+    .then(msg => console.log(msg))
+    .catch(err => console.log(err));
+
+checkAge(14)
+            .then(msg=>console.log("allowed"))
+            .catch(err=>console.log("not allowed"))
+
+
+
+
+
